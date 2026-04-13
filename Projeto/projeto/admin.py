13ddfa6984c68da2b34dd4c0ba4cc33383ede00d
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2026-04-13 10:29:34 -0300

    aula 13/04/26

--- Projeto/projeto/admin.py
+++ Projeto/projeto/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-
+# senha: 702142mh
 from .models import * #imporata nossos models
 admin.site.register(Fabricante) #adiciona a interface do adm
+admin.site.register(Categoria)
+admin.site.register(Produto)
