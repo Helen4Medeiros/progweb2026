eb8ac18a587cba55dadd6e008c03d017f492c541
Helen4Medeiros <cunha.medeiros@escolar.ifrn.edu.br>
2026-04-24 10:15:38 -0300

    commit 24/04/26

--- Projeto/projeto/admin.py
+++ Projeto/projeto/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 # senha: 702142mh
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+
+class FabricanteAdmin(admin.ModelAdmin):
+# Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Fabricante,FabricanteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+
