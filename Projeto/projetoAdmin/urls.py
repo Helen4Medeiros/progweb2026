1020890a56272f86a8c42dbe01f11456c45d1b01
Helen4Medeiros <>
2026-07-10 09:57:24 -0300

    commit 10/07

--- Projeto/projetoAdmin/urls.py
+++ Projeto/projetoAdmin/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projeto.urls.HomeUrls')),
     path('produto/', include('projeto.urls.ProdutoUrls')),
-    path('usuario/', include('projeto.urls.UsuariosUrls')),
+    path('usuario/', include('projeto.urls.UsuarioUrls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
